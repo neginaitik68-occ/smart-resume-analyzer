fa6514e951f6456abd06362d306753eb84793736
neginaitik68-occ <neginaitik68@gmail.com>
2026-09-04 14:16:54 +0530

    Day 2: Add resume PDF upload and text extraction

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pypdf import PdfReader
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    extracted_text = ""
+
+    if request.method == "POST":
+
+        file = request.files["resume"]
+
+        if file and file.filename.endswith(".pdf"):
+
+            file_path = os.path.join(
+                app.config["UPLOAD_FOLDER"],
+                file.filename
+            )
+
+            file.save(file_path)
+
+            reader = PdfReader(file_path)
+
+            for page in reader.pages:
+                text = page.extract_text()
+
+                if text:
+                    extracted_text += text
+
+    return render_template(
+        "index.html",
+        extracted_text=extracted_text
+    )
 
 
 if __name__ == "__main__":
